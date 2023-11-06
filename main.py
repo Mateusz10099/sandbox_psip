ebdef5b70ccfda2a49d433f45606910d17be2e28
Mateusz10099 <149382121+Mateusz10099@users.noreply.github.com>
2023-11-06 12:56:29 +0100

    chore:add create funcionality

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-users_list:list = [
-    {"name":"Marek", "nick":"major","posts":100},
-    {"name":"Mateusz", "nick":"świetlik","posts":60},
-    {"name":"Bartosz", "nick":"Baran","posts":31231},
-    {"name":"Kamil", "nick":"Koc","posts":123},
-    {"name":"Marek", "nick":"Wisse","posts":0},
-    {"name":"Wiktoria", "nick":"Witka","posts":34},
-    {"name":"Wiktoria", "nick":"Wiki","posts":360},
-    {"name":"Monika", "nick":"Monia","posts":100_000_000},
-    {"name":"Kasia", "nick":"Kkk","posts":1_500}
-]
+from dane import users_list
+
+def add_user_to(users_list: list) -> None:
+    """
+    add object to list
+    :param users_list: list - user list
+    :return: None
+    """
+    name = input('podaj imie ?')
+    posts = input('podaj liczbe postów ?')
+    users_list.append({'name': name, 'posts': posts})
+
+add_user_to(users_list)
+add_user_to(users_list)
+add_user_to(users_list)
 
 for user in users_list:
-    print(f'Twój znajomy {user["name"]} dodał {user["posts"]} postów')
-
-#print(f'Twój znajomy {users_list[0]["nick"]} opublikował {users_list[0]["posts"]}!!!')
+    print(f'Twój znajomy {user["name"]} dodał {user["posts"]}')
