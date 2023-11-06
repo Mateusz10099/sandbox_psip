7d7f192e60dcf1f6b138c770a82891abf04dfb67
Mateusz10099 <149382121+Mateusz10099@users.noreply.github.com>
2023-11-06 14:49:16 +0100

    chore: add gui

--- main.py
+++ main.py
@@ -1,39 +1,6 @@
 from dane import users_list
+from utils.my_functions import gui
 
-def add_user_to(users_list: list) -> None:
-    """
-    add object to list
-    :param users_list: list - user list
-    :return: None
-    """
-    name = input('podaj imie ?')
-    posts = input('podaj liczbe postów ?')
-    users_list.append({'name': name, 'posts': posts})
-
-def remove_user_from(users_list: list) -> None:
-    """
-    remove object from list
-    :param users_list: list - user list
-    :return: None
-    """
-    tmp_list = []
-    name = input('podaj imie użytkownika do usunięcia: ')
-    for user in users_list:
-        if user["name"] == name:
-            tmp_list.append(user)
-    print(f'Znaleziono użytkowników :')
-    print('0. Usuń wszystkich znalezionych użytkowników')
-    for numerek, user_to_be_removed in enumerate(tmp_list):
-        print(f'{numerek+1}. {user_to_be_removed}')
-    numer = int(input(f'wybierz numer użytkownika do usunięcia: '))
-    if numer == 0:
-        for user in tmp_list:
-            users_list.remove(user)
-    else:
-        users_list.remove(tmp_list[numer-1])
+gui(users_list)
 
 
-#add_user_to(users_list)
-
-for user in users_list:
-    print(f'Twój znajomy {user["name"]} dodał {user["posts"]}')
