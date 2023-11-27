3c6eade9609398d4b66ea801d14edb4fe17cefe4
Mateusz10099 <149382121+Mateusz10099@users.noreply.github.com>
2023-11-27 13:14:52 +0100

    chore: get_coordinates from wiki page

--- notes.py
+++ notes.py
@@ -1,17 +1,29 @@
-print("Fun fact: The cracking sound a bullwhip makes when properly wielded is, in fact, a small sonic boom. The end of the whip, known as the cracker, moves faster than the speed of sound, thus creating a sonic boom. "
-      "The whip is probably the first human invention to break the sound barrier.")
-from dane import users_list
+# BEAUTIFULSOUP4
 
-def update_user(users_list: list [dict,dict]) -> None:
- nick_of_user = input("Prosze o podanie pseudnimu użytkownika którego chcesz zmodyfikować ")
- print(nick_of_user)
- for user in users_list:
-    if user['nick'] == nick_of_user:
-     print("FOUND")
-     user['name'] = input('Podaj nowe imie: ')
-     user['nick'] = input('Podaj nowy pseudonim: ')
-     user['posts'] = int(input('Podaj liczbe postow: '))
+from bs4 import BeautifulSoup
+import requests
+import re
 
-update_user(users_list)
-for user in users_list:
-         print(user)
+#-*- coding: utf-8 -*- TO JEST KOD NA ZAMIANĘ ZNAKÓW POLSKICH
+
+nazwy_miejscowosci = ['Opoczno','Lublin','Gdańsk']
+def get_coordinates_of(city:str)->list[float,float]:
+    # pobrane strony internetowe
+
+    adres_URL = f'https://pl.wikipedia.org/wiki/{city}'
+
+    response = requests.get(url=adres_URL)
+    response_html = BeautifulSoup(response.text, 'html.parser')
+
+    # pobranie współrzędnych z treści strony internetowej
+
+    response_html_latitude = (response_html.select('.latitude')[1].text) # . ponieważ class
+    response_html_latitude = float(response_html_latitude.replace(',','.'))
+    response_html_longitude = (response_html.select('.longitude')[1].text) # . ponieważ class
+    response_html_longitude = float(response_html_longitude.replace(',','.'))
+
+    return [response_html_latitude, response_html_longitude]
+
+for item in nazwy_miejscowosci:
+    print(get_coordinates_of(item))
+
