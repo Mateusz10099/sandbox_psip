e9f11ac51a92a199877f6eec6f7307d77eb250e9
Mateusz10099 <149382121+Mateusz10099@users.noreply.github.com>
2023-12-04 15:34:44 +0100

    feat: upgrade_po_zajeciach

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from utils.my_functions import gui
 
 gui(users_list)
 
-
+#  TODO włączyć do kodu aktualnego obsługę bazy danych
+#  TODO dodać tabelę do bazy danych reprezentującą użytkownika
+#  TODO napisać klasę użytkownika o strukturze zgodnej z tą zdefiniowanej w my_data
--- notes.py
+++ notes.py
@@ -1,19 +1,33 @@
-import requests
+import sqlalchemy
+
+db_params = sqlalchemy.URL.create(
+    drivername="postgresql+psycopg2",
+    username= "postgres",
+    password= "Monika",
+    host= "localhost",
+    database= "postgres",
+    port= 5433
+)
 
 
-class User:
-    def __init__(self, miasto):
-        self.miasto = miasto
-    def pogoda_z(self, miasto: str):
-        url = f"https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}"
-        return requests.get(url).json()
+engine = sqlalchemy.create_engine(db_params)
+connection= engine.connect()
 
-npc_1=User( miasto="warszawa")
-npc_2=User( miasto="zamosc")
+#sql_query_1=sqlalchemy.text("INSERT INTO public.my_table(name) VALUES('kepa');")
+#sql_query_1=sqlalchemy.text("select * from public.my_table;")
+#user = input('podaj nazwę zawodnika do usunięcia')
+#sql_query_1=sqlalchemy.text(f"DELETE FROM public.my_table where name='{user}';")
+#kogo_zmienic=input('podaj kogo zmienic')
+#na_kogo=input('podaj na kogo zamienic')
+#sql_query_1=sqlalchemy.text(f"update public.my_table set name='{na_kogo}' where name = '{kogo_zamienic}';")
 
-print(npc_1.miasto)
-print(npc_2.miasto)
-print(npc_1.pogoda_z(npc_1.miasto))
-print(npc_2.pogoda_z(npc_2.miasto))
+def dodaj_uzytkownika(user:str):
+    sql_query_1 = sqlalchemy.text(f"INSERT INTO public.my_table(name) VALUES '{user}';")
+    connection.execute(sql_query_1)
+    connection.commit()
+cwok='stasiu'
+dodaj_uzytkownika(cwok)
 
-pogoda_z("warszawa")
+#connection.execute(sql_query_1)
+#connection.commit()
+
